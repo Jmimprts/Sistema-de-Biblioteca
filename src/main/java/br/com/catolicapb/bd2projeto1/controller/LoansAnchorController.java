5fa38403544b2a867f8e71bb8d07b3cefd14ac14
vonderheidon <jefferson.sg91@hotmail.com>
2025-03-18 12:55:25 -0300

    tabelas adicionadas

--- src/main/java/br/com/catolicapb/bd2projeto1/controller/LoansAnchorController.java
+++ src/main/java/br/com/catolicapb/bd2projeto1/controller/LoansAnchorController.java
@@ -1,4 +1,15 @@
 package br.com.catolicapb.bd2projeto1.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+
 public class LoansAnchorController {
+
+    @FXML
+    private ChoiceBox<String> cbFilter;
+
+    public void initialize() {
+        cbFilter.getItems().addAll("Exibir todos", "Exibir empréstimos pendentes", "Exibir empréstimos atrasados");
+        cbFilter.setValue("Exibir todos");
+    }
 }
--- src/main/java/br/com/catolicapb/bd2projeto1/controller/ReadersAnchorController.java
+++ src/main/java/br/com/catolicapb/bd2projeto1/controller/ReadersAnchorController.java
@@ -1,4 +1,15 @@
 package br.com.catolicapb.bd2projeto1.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+
 public class ReadersAnchorController {
+
+    @FXML
+    private ChoiceBox<String> cbFilter;
+
+    public void initialize() {
+        cbFilter.getItems().addAll("Exibir todos", "Exibir leitores com mais de um empréstimo pendente", "Exibir leitores que nunca fizeram empréstimos");
+        cbFilter.setValue("Exibir todos");
+    }
 }
