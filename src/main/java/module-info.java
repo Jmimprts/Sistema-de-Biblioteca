ff3a3163f67a6df60ce8b96ee3f41f43cf2c79be
Jefferson <jefferson.sg91@hotmail.com>
2025-03-18 15:36:38 -0300

    correção do module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,8 +3,11 @@ module br.com.catolicapb.bd2projeto1 {
     requires javafx.fxml;
     requires jakarta.persistence;
     requires static lombok;
+    requires org.hibernate.orm.core;
 
     opens br.com.catolicapb.bd2projeto1.controller to javafx.fxml;
+    opens br.com.catolicapb.bd2projeto1.entity to org.hibernate.orm.core, jakarta.persistence;
+
     exports br.com.catolicapb.bd2projeto1.util;
     exports br.com.catolicapb.bd2projeto1.controller;
 }
