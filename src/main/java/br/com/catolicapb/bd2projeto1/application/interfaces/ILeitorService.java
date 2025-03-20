38c5f94947b137cd86566fc9ab2e5cb40d3521be
eutawan <principalacc169@gmail.com>
2025-03-19 21:37:47 -0300

    métodos obrigatórios da classe Leitor adicionados

--- src/main/java/br/com/catolicapb/bd2projeto1/application/interfaces/ILeitorService.java
+++
@@ -1,5 +0,0 @@
-package br.com.catolicapb.bd2projeto1.application.interfaces;
-
-public interface ILeitorService {
-    public void cadastrarleitor();
-}
--- src/main/java/br/com/catolicapb/bd2projeto1/infrastructure/Repositories/LeitorRepository.java
+++ src/main/java/br/com/catolicapb/bd2projeto1/infrastructure/Repositories/LeitorRepository.java
@@ -1,8 +1,15 @@
 package br.com.catolicapb.bd2projeto1.infrastructure.Repositories;
 
+import br.com.catolicapb.bd2projeto1.entity.Emprestimo;
 import br.com.catolicapb.bd2projeto1.entity.Leitor;
 import br.com.catolicapb.bd2projeto1.infrastructure.loaders.DataLoader;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.Root;
+
+import java.util.List;
 
 public class LeitorRepository {
     public void addLeitor(Leitor leitor) {
@@ -11,5 +18,34 @@ public class LeitorRepository {
         em.persist(leitor);
         em.getTransaction().commit();
         em.close();
+    }
+
+    public int consultarTotalEmprestimoPorLeitor(Long idLeitor) {
+        EntityManager em = DataLoader.getEntityManager();
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Long> query = cb.createQuery(Long.class);
+
+        Root<Emprestimo> root = query.from(Emprestimo.class);
+        query.select(cb.count(root))
+                .where(cb.equal(root.get("leitor").get("id"), idLeitor));
+
+        Long resultado = em.createQuery(query).getSingleResult();
+        return resultado != null ? resultado.intValue() : 0;
+    }
+
+    public List<Leitor> buscarLeitoresComEmprestimosPendente() {
+        EntityManager em = DataLoader.getEntityManager();
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Leitor> query = cb.createQuery(Leitor.class);
+
+        Root<Emprestimo> root = query.from(Emprestimo.class);
+        Join<Emprestimo, Leitor> leitorJoin = root.join("leitor");
+
+        query.select(leitorJoin)
+                .groupBy(leitorJoin.get("id"), leitorJoin.get("nome"))
+                .having(cb.greaterThan(cb.count(root), cb.literal(1L)))
+                .where(cb.equal(root.get("status"), "PENDENTE"));
+
+        return em.createQuery(query).getResultList();
     }
 }
